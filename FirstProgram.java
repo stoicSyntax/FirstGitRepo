7d3ef0c7a16115f7211d64cee3efde109f2fc64e
nishant171298@gmail.com <nishant171298@gmail.com>
2022-08-18 16:25:35 +0530

    Second java program in same repo

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+package String;
+
+public class FirstProgram {
+	public static void main(String[] args) { 
+	int res=max(3,5);
+	System.out.println(res);
+		
+	}
+	
+	static  int max(int a, int b) {
+		if(a>b) {
+			return a;
+		}else return b;  
+		
+	}
+}
