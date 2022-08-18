bbb5836801731668ae55261814d0858bdf3a4984
nishant171298@gmail.com <nishant171298@gmail.com>
2022-08-18 17:25:43 +0530

    Added school in the info

--- app.java
+++ app.java
@@ -1,22 +1,21 @@
 
-//import java.util.Scanner;
+import java.util.Scanner;
 public class app{
 
 	public static void main(String[]args) {
 		
-		/*Scanner myvar=new Scanner(System.in);
+		Scanner myvar=new Scanner(System.in);
 		
 		System.out.print("Enter your name: ");
-		String name=myvar.nextLine();
+		String name = myvar.nextLine();
+		
+		System.out.print("Enter your School: ");
+		String school = myvar.nextLine();
 		
 		System.out.print("Enter your age: ");
-		int age=myvar.nextInt();
-		
-		System.out.println("Hey "+name +"! you are "+age+"years old");*/
-		
-		 
-		
-		
-	}
+		int age = myvar.nextInt();
 
+       System.out.println("Hey "+name +"! you are "+age+" years old. "+ "you learn at "+school +".");
+		
+		myvar.close();}
 }
