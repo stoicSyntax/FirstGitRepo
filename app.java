0b5f0a686e91d150d8999abfde064a7729419241
nishant171298@gmail.com <nishant171298@gmail.com>
2022-08-18 11:08:33 +0530

    first github commit

---
+++ app.java
@@ -0,0 +1,22 @@
+
+//import java.util.Scanner;
+public class app{
+
+	public static void main(String[]args) {
+		
+		/*Scanner myvar=new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+		String name=myvar.nextLine();
+		
+		System.out.print("Enter your age: ");
+		int age=myvar.nextInt();
+		
+		System.out.println("Hey "+name +"! you are "+age+"years old");*/
+		
+		 
+		
+		
+	}
+
+}
