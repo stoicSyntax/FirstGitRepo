7391c3dfc6fa53270b9fc7d51029f2ce00ccc15a
nishant171298@gmail.com <nishant171298@gmail.com>
2023-06-28 10:52:58 +0530

    Revert "address and academics"
    This reverts commit 7972d70125eb2eb675fb4a44f7d2a56bb25e2878.

--- app.java
+++ app.java
@@ -4,24 +4,18 @@ public class app{
 
 	public static void main(String[]args) {
 		
-		Scanner sc=new Scanner(System.in);
+		Scanner myvar=new Scanner(System.in);
 		
 		System.out.print("Enter your name: ");
-		String name = sc.nextLine();
+		String name = myvar.nextLine();
 		
 		System.out.print("Enter your School: ");
-		String school = sc.nextLine();
+		String school = myvar.nextLine();
 		
 		System.out.print("Enter your age: ");
-		int age = sc.nextInt();
-		
-		System.out.println("Enter your address: ");
-		String address = sc.nextLine();
-		
-		System.out.println("Enter your academics: ");
-		String academics = sc.nextLine();
+		int age = myvar.nextInt();
 
        System.out.println("Hey "+name +"! you are "+age+" years old. "+ "you learn at "+school +".");
 		
-		sc.close();}
+		myvar.close();}
 }
