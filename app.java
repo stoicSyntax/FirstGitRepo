aa5184c5e896024de08f4acea3f96500872bd5c2
nishant171298@gmail.com <nishant171298@gmail.com>
2023-06-28 10:55:47 +0530

    Revert "Added school in the info"
    This reverts commit bbb5836801731668ae55261814d0858bdf3a4984.

--- app.java
+++ app.java
@@ -1,21 +1,22 @@
 
-import java.util.Scanner;
+//import java.util.Scanner;
 public class app{
 
 	public static void main(String[]args) {
 		
-		Scanner myvar=new Scanner(System.in);
+		/*Scanner myvar=new Scanner(System.in);
 		
 		System.out.print("Enter your name: ");
-		String name = myvar.nextLine();
-		
-		System.out.print("Enter your School: ");
-		String school = myvar.nextLine();
+		String name=myvar.nextLine();
 		
 		System.out.print("Enter your age: ");
-		int age = myvar.nextInt();
-
-       System.out.println("Hey "+name +"! you are "+age+" years old. "+ "you learn at "+school +".");
+		int age=myvar.nextInt();
 		
-		myvar.close();}
+		System.out.println("Hey "+name +"! you are "+age+"years old");*/
+		
+		 
+		
+		
+	}
+
 }
